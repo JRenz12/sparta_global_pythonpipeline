a38ecdfd63d14ba584712799b0591049b68c4692
JRenz12 <jrenzulli@spartglobal.com>
2018-07-30 14:41:23 +0100

    Python cookbook

--- spec/unit/recipes/python_spec.rb
+++ spec/unit/recipes/python_spec.rb
@@ -1,22 +1,20 @@
-#
-# Cookbook:: python
-# Spec:: default
-#
-# Copyright:: 2018, The Authors, All Rights Reserved.
-
 require 'spec_helper'
 
-describe 'python::default' do
+describe 'python::python' do
   context 'When all attributes are default, on Ubuntu 16.04' do
     let(:chef_run) do
       # for a complete list of available platforms and versions see:
       # https://github.com/customink/fauxhai/blob/master/PLATFORMS.md
-      runner = ChefSpec::ServerRunner.new(platform: 'ubuntu', version: '16.04')
+      runner = ChefSpec::SoloRunner.new(platform: 'ubuntu', version: '16.04')
       runner.converge(described_recipe)
     end
 
     it 'converges successfully' do
       expect { chef_run }.to_not raise_error
+    end
+
+    it 'should install Python' do
+      expect( chef_run ).to install_package 'python'
     end
   end
 end
